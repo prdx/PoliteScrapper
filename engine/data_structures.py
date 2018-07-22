9a579a14dbcbe3ee8b0020a678021ef237301d17
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-07-22 15:04:01 -0400

    Update data structure'

---
+++ engine/data_structures.py
@@ -0,0 +1,42 @@
+import heapq
+import time
+
+class Heap(object):
+    """Max heap (Priority queue) implementation by inversing the value in min heap
+    To simplify, we will be using heapq. But heapq supports only min-heap.
+    Then we just simply multiply the value by -1
+    """
+
+    def __init__(self):
+        self.heap = []
+
+    def push(self, value):
+        heapq.heappush(self.heap, value)
+    
+    def pop(self):
+        if self.heap == []: return None
+        else: return heapq.heappop(self.heap)
+
+    def heapify(self):
+        heapq.heapify(self.heap)
+        return self.heap
+
+    def size(self):
+        return len(self.heap)
+
+
+class queue_element(object):
+    def __init__(self, url, inlinks):
+            self.url = url
+            self.inlinks = -inlinks
+            self.timestamp = time.time()
+
+    # FIXME
+    def increase_inlinks(self, delta):
+            self.inlinks -= delta
+
+    def __lt__(self, other):
+        if self.inlinks == other.inlinks:
+            return self.timestamp < other.timestamp
+        return self.inlinks < other.inlinks
+
