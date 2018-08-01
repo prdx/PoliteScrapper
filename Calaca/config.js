e84a49fa197e484361d2e69421b32fd4240c884c
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-08-01 17:48:14 -0400

    Add modified calca

---
+++ Calaca/config.js
@@ -0,0 +1,6 @@
+/* Configs */
+url: "http://localhost:9200" //Cluster http url
+index_name: "merged" //Index name or comma-separated list
+type: "document" //Type
+size: 10 //Number of results displayed at a time
+search_delay: 500 //Delay between actual search request in ms
